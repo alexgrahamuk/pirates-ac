1923a53a0c01db23afd68daa3fa61b21ebf8fdfd
alex <alex@a-graham.com>
2021-01-27 11:35:45 +0000

    Pre production changes, logic implemented including cookies

--- public/js/piratesac-public.js
+++ public/js/piratesac-public.js
@@ -1,77 +1,99 @@
+/*! js-cookie v3.0.0-rc.1 | MIT */
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e=e||self,function(){var n=e.Cookies,r=e.Cookies=t();r.noConflict=function(){return e.Cookies=n,r}}())}(this,function(){"use strict";function e(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var r in n)e[r]=n[r]}return e}var t={read:function(e){return e.replace(/(%[\dA-F]{2})+/gi,decodeURIComponent)},write:function(e){return encodeURIComponent(e).replace(/%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,decodeURIComponent)}};return function n(r,o){function i(t,n,i){if("undefined"!=typeof document){"number"==typeof(i=e({},o,i)).expires&&(i.expires=new Date(Date.now()+864e5*i.expires)),i.expires&&(i.expires=i.expires.toUTCString()),t=encodeURIComponent(t).replace(/%(2[346B]|5E|60|7C)/g,decodeURIComponent).replace(/[()]/g,escape),n=r.write(n,t);var c="";for(var u in i)i[u]&&(c+="; "+u,!0!==i[u]&&(c+="="+i[u].split(";")[0]));return document.cookie=t+"="+n+c}}return Object.create({set:i,get:function(e){if("undefined"!=typeof document&&(!arguments.length||e)){for(var n=document.cookie?document.cookie.split("; "):[],o={},i=0;i<n.length;i++){var c=n[i].split("="),u=c.slice(1).join("=");'"'===u[0]&&(u=u.slice(1,-1));try{var f=t.read(c[0]);if(o[f]=r.read(u,f),e===f)break}catch(e){}}return e?o[e]:o}},remove:function(t,n){i(t,"",e({},n,{expires:-1}))},withAttributes:function(t){return n(this.converter,e({},this.attributes,t))},withConverter:function(t){return n(e({},this.converter,t),this.attributes)}},{attributes:{value:Object.freeze(o)},converter:{value:Object.freeze(r)}})}(t,{path:"/"})});
+
 var pacbar =
 {
 	erf: false,
 	da: false,
 	dc: false,
 	dbi: false,
-	ra: false,
+	oc: false,
 
 	//Bind
 	init: function()
 	{
-		$(".pacbar ul li").on("click", "a", function()
+		pacbar.pacbar_read();
+		pacbar.pacbar_update();
+
+		jQuery(".pacbar ul li").on("click", "a", function()
 		{
-			if ($(this).hasClass("pacbar-erf"))
-				pacbar.pacbar_erf();
-			else if ($(this).hasClass("pacbar-da"))
-				pacbar.pacbar_da();
-			else if ($(this).hasClass("pacbar-dc"))
-				pacbar.pacbar_dc();
-			else if ($(this).hasClass("pacbar-dbi"))
-				pacbar.pacbar_dbi();
-			else if ($(this).hasClass("pacbar-ra"))
+			if (jQuery(this).hasClass("pacbar-erf"))
+				pacbar.erf = !(pacbar.erf);
+			else if (jQuery(this).hasClass("pacbar-da"))
+				pacbar.da = !(pacbar.da);
+			else if (jQuery(this).hasClass("pacbar-dc"))
+				pacbar.dc = !(pacbar.dc);
+			else if (jQuery(this).hasClass("pacbar-dbi"))
+				pacbar.dbi = !(pacbar.dbi);
+			else if (jQuery(this).hasClass("pacbar-ra"))
 				pacbar.pacbar_ra();
-			else if ($(this).hasClass("pacbar-toggle"))
-				pacbar.pacbar_toggle();
+
+			pacbar.pacbar_write();
+			pacbar.pacbar_update();
 		});
 
+		jQuery(".pacbar .pacbar-control").on("click", "a", function()
+		{
+			//Refactor
+			pacbar.pacbar_toggle();
+			pacbar.pacbar_write();
+			pacbar.pacbar_update();
+		});
+
+
+
 		return false;
+	},
+
+	pacbar_update: function()
+	{
+		jQuery(".pacbar ul li a.pacbar-erf").removeClass("enabled");
+		if (pacbar.erf === true)
+			jQuery(".pacbar ul li a.pacbar-erf").addClass("enabled");
+
+		jQuery(".pacbar ul li a.pacbar-da").removeClass("enabled");
+		if (pacbar.da === true)
+			jQuery(".pacbar ul li a.pacbar-da").addClass("enabled");
+
+		jQuery(".pacbar ul li a.pacbar-dc").removeClass("enabled");
+		if (pacbar.dc === true)
+			jQuery(".pacbar ul li a.pacbar-dc").addClass("enabled");
+
+		jQuery(".pacbar ul li a.pacbar-dbi").removeClass("enabled");
+		if (pacbar.dbi === true)
+			jQuery(".pacbar ul li a.pacbar-dbi").addClass("enabled");
+
+		if (pacbar.oc === true)
+			jQuery(".pacbar").removeClass("closed");
+		else
+			jQuery(".pacbar").addClass("closed");
+
 	},
 
 	//Toggle Enable Readable Font
 	pacbar_erf: function()
 	{
-		if (pacbar.ra === false)
-			pacbar.erf = !(pacbar.erf);
 
-		$(".pacbar ul li a.pacbar-erf").removeClass("enabled");
-		if (pacbar.erf === true)
-			$(".pacbar ul li a.pacbar-erf").addClass("enabled");
 	},
 
-	//Toggle Disable Adnimations
+	//Toggle Disable Animations
 	pacbar_da: function()
 	{
-		if (pacbar.ra === false)
-			pacbar.da = !(pacbar.da);
 
-		$(".pacbar ul li a.pacbar-da").removeClass("enabled");
-		if (pacbar.erf === true)
-			$(".pacbar ul li a.pacbar-da").addClass("enabled");
 
 	},
 
 	//Toggle Columns
 	pacbar_dc: function()
 	{
-		if (pacbar.ra === false)
-			pacbar.dc = !(pacbar.dc);
 
-		$(".pacbar ul li a.pacbar-dc").removeClass("enabled");
-		if (pacbar.erf === true)
-			$(".pacbar ul li a.pacbar-dc").addClass("enabled");
 
 	},
 
 	//Toggle Background Images
 	pacbar_dbi: function()
 	{
-		if (pacbar.ra === false)
-			pacbar.dbi = !(pacbar.dbi);
 
-		$(".pacbar ul li a.pacbar-dbi").removeClass("enabled");
-		if (pacbar.erf === true)
-			$(".pacbar ul li a.pacbar-dbi").addClass("enabled");
 
 	},
 
@@ -82,26 +104,41 @@ var pacbar =
 		pacbar.da = false;
 		pacbar.dc = false;
 		pacbar.dbi = false;
-		pacbar.ra = true;
 
-		pacbar.erf();
-		pacbar.da();
-		pacbar.dc();
-		pacbar.dbi();
+		pacbar.pacbar_write();
+		pacbar.pacbar_update();
 
-		pacbar.ra = false;
+	},
+
+	pacbar_write: function()
+	{
+		Cookies.set("pb_erf", pacbar.erf);
+		Cookies.set("pb_da", pacbar.da);
+		Cookies.set("pb_dc", pacbar.dc);
+		Cookies.set("pb_dbi", pacbar.dbi);
+		Cookies.set("pb_oc", pacbar.oc);
+	},
+
+	pacbar_read: function()
+	{
+		pacbar.erf = (Cookies.get("pb_erf") == "true") ? true : false;
+		pacbar.da = (Cookies.get("pb_da") == "true") ? true : false;
+		pacbar.dc = (Cookies.get("pb_dc") == "true") ? true : false;
+		pacbar.dbi = (Cookies.get("pb_dbi") == "true") ? true : false;
+		pacbar.oc = (Cookies.get("pb_oc") == "true") ? true : false;
 	},
 
 	//Handle open and close, cookies handled in init
 	pacbar_toggle: function()
 	{
-
+		//Anim?
+		pacbar.oc = !(pacbar.oc);
+		pacbar.pacbar_write();
+		pacbar.pacbar_update();
 	}
 };
 
-
-(function($)
+jQuery(document).ready(function($)
 {
-	'use strict';
 	pacbar.init();
-})( jQuery );
+});
